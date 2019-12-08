2f911ad51ef3326ce7e6f1ae8b081d3ffa23d27d
Edgar <ms724472@iteso.mx>
2019-12-08 14:33:29 -0600

    Modificando estilo columnas mediciones

--- src/js/viewModels/evalIndv.js
+++ src/js/viewModels/evalIndv.js
@@ -33,15 +33,15 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojdatetimepicker', 'ojs/ojarray
             ]);
 
             self.columnasMediciones = ko.observableArray([
-                {headerText: 'Fecha medición', field: 'fecha'},
-                {headerText: 'Masa', field: 'masa'},
-                {headerText: 'Estatura', field: 'estatura'},
-                {headerText: 'Indice de Masa Coporal', field: 'imc'},
-                {headerText: 'Perimetro del Cuello', field: 'perimetro_cuello'},
-                {headerText: 'Cintura', field: 'cintura'},
-                {headerText: 'Triceps', field: 'triceps'},
-                {headerText: 'Subescapula', field: 'subescapula'},
-                {headerText: 'Pliegue del Cuello', field: 'pliegue_cuello'}
+                {headerText: 'Fecha medición', field: 'fecha', style: 'text-align: right;'},
+                {headerText: 'Masa', field: 'masa', style: 'text-align: right;'},
+                {headerText: 'Estatura', field: 'estatura', style: 'text-align: right;'},
+                {headerText: 'Indice de Masa Coporal', field: 'imc', style: 'text-align: right;'},
+                {headerText: 'Perimetro del Cuello', field: 'perimetro_cuello', style: 'text-align: right;'},
+                {headerText: 'Cintura', field: 'cintura', style: 'text-align: right;'},
+                {headerText: 'Triceps', field: 'triceps', style: 'text-align: right;'},
+                {headerText: 'Subescapula', field: 'subescapula', style: 'text-align: right;'},
+                {headerText: 'Pliegue del Cuello', field: 'pliegue_cuello', style: 'text-align: right;'}
             ]);
 
             self.origenDatosAlumnos = ko.observable();
