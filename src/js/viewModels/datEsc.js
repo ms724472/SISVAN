4597f4664ac8f18ff7c7d64722cf83864e40f332
Edgar Tiburcio <edgar.tiburcio@oracle.com>
2020-10-03 20:43:54 -0500

    Agregando nuevo diaologo y logica para agregar escuelas

--- src/js/viewModels/datEsc.js
+++ src/js/viewModels/datEsc.js
@@ -5,9 +5,9 @@
 /*
  * Your about ViewModel code goes here
  */
-define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojmenu', 'ojs/ojtable', 'ojs/ojarraytabledatasource',
+define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojarraydataprovider', 'ojs/ojmenu', 'ojs/ojtable', 'ojs/ojarraytabledatasource',
 'ojs/ojpagingtabledatasource', 'ojs/ojpagingcontrol', 'ojs/ojbutton', 'ojs/ojtoolbar'],
- function(oj, ko, $) {
+ function(oj, ko, $, ArrayDataProvider) {
   
     function AboutViewModel() {
       var self = this;
@@ -16,6 +16,16 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojmenu', 'ojs/ojtable', 'ojs/oj
 
       self.origenDatosEscuelas = ko.observable();
       self.origenDatosGrupos = ko.observable();
+      self.campoCCT = ko.observable();
+      self.campoNombre = ko.observable();
+      self.campoDireccion = ko.observable();
+      self.campoColonia = ko.observable();
+      self.campoCodigoPostal = ko.observable();
+      self.campoTelefono = ko.observable();
+      self.campoEstado = ko.observable();
+      self.campoMunicipio = ko.observable();
+      self.tituloDialogoEscuela = ko.observable("Agregar nueva escuela");
+      self.botonDialogoEscuela = ko.observable("Agregar");
       
       self.nombresColumnas = ko.observableArray([
         { headerText: 'Nombre', field: 'nombre' },
@@ -33,6 +43,150 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojmenu', 'ojs/ojtable', 'ojs/oj
         { headerText: 'Año de ingreso', field: 'anio_ingreso' },
         { headerText: 'Año de egreso', field: 'anio_graduacion' }
       ]);
+
+      self.estados = [
+        { value: 'Jalisco' }
+      ];
+      
+      self.datosEstados = new ArrayDataProvider(self.estados, {keyAttributes: 'value'});
+      self.datosMunicipios = ko.observableArray();
+
+      self.municipios = {
+        Jalisco: [
+          { value: 'Acatic' },
+          { value: 'Acatlán de Juárez' },
+          { value: 'Ahualulco de Mercado' },
+          { value: 'Amacueca' },
+          { value: 'Amatitán' },
+          { value: 'Ameca' },
+          { value: 'Arandas' },
+          { value: 'Atemajac de Brizuela' },
+          { value: 'Atengo' },
+          { value: 'Atenguillo' },
+          { value: 'Atotonilco el Alto' },
+          { value: 'Atoyac' },
+          { value: 'Autlán de Navarro' },
+          { value: 'Ayotlán' },
+          { value: 'Ayutla' },
+          { value: 'Bolaños' },
+          { value: 'Cabo Corrientes' },
+          { value: 'Cañadas de Obregón' },
+          { value: 'Casimiro Castillo' },
+          { value: 'Chapala' },
+          { value: 'Chimaltitán' },
+          { value: 'Chiquilistlán' },
+          { value: 'Cihuatlán' },
+          { value: 'Cocula' },
+          { value: 'Colotlán' },
+          { value: 'Concepción de Buenos Aires' },
+          { value: 'Cuautitlán de García Barragán' },
+          { value: 'Cuautla' },
+          { value: 'Cuquío' },
+          { value: 'Degollado' },
+          { value: 'Ejutla' },
+          { value: 'El Arenal' },
+          { value: 'El Grullo' },
+          { value: 'El Limón' },
+          { value: 'El Salto' },
+          { value: 'Encarnación de Díaz' },
+          { value: 'Etzatlán' },
+          { value: 'Gómez Farías' },
+          { value: 'Guachinango' },
+          { value: 'Guadalajara' },
+          { value: 'Hostotipaquillo' },
+          { value: 'Huejúcar' },
+          { value: 'Huejuquilla el Alto' },
+          { value: 'Ixtlahuacán de los Membrillos' },
+          { value: 'Ixtlahuacán del Río' },
+          { value: 'Jalostotitlán' },
+          { value: 'Jamay' },
+          { value: 'Jesús María' },
+          { value: 'Jilotlán de los Dolores' },
+          { value: 'Jocotepec' },
+          { value: 'Juanacatlán' },
+          { value: 'Juchitlán' },
+          { value: 'La Barca' },
+          { value: 'La Huerta' },
+          { value: 'La Manzanilla de la Paz' },
+          { value: 'Lagos de Moreno' },
+          { value: 'Magdalena' },
+          { value: 'Mascota' },
+          { value: 'Mazamitla' },
+          { value: 'Mexticacán' },
+          { value: 'Mezquitic' },
+          { value: 'Mixtlán' },
+          { value: 'Ocotlán' },
+          { value: 'Ojuelos de Jalisco' },
+          { value: 'Pihuamo' },
+          { value: 'Poncitlán' },
+          { value: 'Puerto Vallarta' },
+          { value: 'Quitupan' },
+          { value: 'San Cristóbal de la Barranca' },
+          { value: 'San Diego de Alejandría' },
+          { value: 'San Gabriel' },
+          { value: 'San Ignacio Cerro Gordo' },
+          { value: 'San Juan de los Lagos' },
+          { value: 'San Juanito de Escobedo' },
+          { value: 'San Julián' },
+          { value: 'San Marcos' },
+          { value: 'San Martín de Bolaños' },
+          { value: 'San Martín de Hidalgo' },
+          { value: 'San Miguel el Alto' },
+          { value: 'San Sebastián del Oeste' },
+          { value: 'Santa María de los Ángeles' },
+          { value: 'Santa María del Oro' },
+          { value: 'Sayula' },
+          { value: 'Tala' },
+          { value: 'Talpa de Allende' },
+          { value: 'Tamazula de Gordiano' },
+          { value: 'Tapalpa' },
+          { value: 'Tecalitlán' },
+          { value: 'Tecolotlán' },
+          { value: 'Techaluta de Montenegro' },
+          { value: 'Tenamaxtlán' },
+          { value: 'Teocaltiche' },
+          { value: 'Teocuitatlán de Corona' },
+          { value: 'Tepatitlán de Morelos' },
+          { value: 'Tequila' },
+          { value: 'Teuchitlán' },
+          { value: 'Tizapán el Alto' },
+          { value: 'Tlajomulco de Zúñiga' },
+          { value: 'Tlaquepaque' },
+          { value: 'Tolimán' },
+          { value: 'Tomatlán' },
+          { value: 'Tonalá' },
+          { value: 'Tonaya' },
+          { value: 'Tonila' },
+          { value: 'Totatiche' },
+          { value: 'Tototlán' },
+          { value: 'Tuxcacuesco' },
+          { value: 'Tuxcueca' },
+          { value: 'Tuxpan' },
+          { value: 'Unión de San Antonio' },
+          { value: 'Unión de Tula' },
+          { value: 'Valle de Guadalupe' },
+          { value: 'Valle de Juárez' },
+          { value: 'Villa Purificación' },
+          { value: 'Villa Corona' },
+          { value: 'Villa Guerrero' },
+          { value: 'Villa Hidalgo' },
+          { value: 'Yahualica de González Gallo' },
+          { value: 'Zacoalco de Torres' },
+          { value: 'Zapopan' },
+          { value: 'Zapotiltic' },
+          { value: 'Zapotitlán de Vadillo' },
+          { value: 'Zapotlán del Rey' },
+          { value: 'Zapotlán El Grande' },
+          { value: 'Zapotlanejo' }
+        ]
+      };
+
+      self.estadoSeleccionado = function (event) {
+        var estado = event['detail'].value;
+        if (estado !== "" && self.municipios.hasOwnProperty(estado) && Object.keys(self.municipios[estado]).length > 0) {
+          self.datosMunicipios(new ArrayDataProvider(self.municipios[estado], {keyAttributes: 'value'}));
+        }
+      };
 
       var peticionDatosEscuelas = new XMLHttpRequest();
       peticionDatosEscuelas.open('GET', oj.gWSUrl() + "obtenerDatosEscuelas", false);
@@ -72,11 +226,83 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojmenu', 'ojs/ojtable', 'ojs/oj
 
       self.escuelaSeleccionada = function(event) {
         console.log(event.target.value);
-      }
+      };
 
       self.grupoSeleccionado = function(event) {
         console.log(event.target.value);
-      }
+      };
+
+      self.crearNuevaEscuela = function(event) {
+        self.tituloDialogoEscuela("Agregar nueva escuela");
+        self.botonDialogoEscuela("Agregar");
+        document.getElementById('dialogoEscuela').open();
+      };
+
+      self.limpiarDialogoEscuela = function(event) {
+        self.campoCCT("");
+        self.campoNombre("");
+        self.campoDireccion("");
+        self.campoColonia("");
+        self.campoCodigoPostal("");
+        self.campoTelefono("");
+        self.campoEstado("");
+        self.campoMunicipio("");
+        document.getElementById('dialogoEscuela').close();
+      };
+
+      self.procesarDatosEscuela = function (event) {
+        var datosEscuelas = {
+          clave_sep: self.campoCCT(),
+          nombre: self.campoNombre(),
+          direccion: self.campoDireccion(),
+          colonia: self.campoColonia(),
+          codigo_postal: self.campoCodigoPostal(),
+          telefono: self.campoTelefono(),
+          estado: self.campoEstado(),
+          municipio: self.campoMunicipio()
+        };
+
+        var servicio = "agregarEscuela";
+        var metodo = "POST";
+
+        if (self.botonDialogoEscuela() !== "Agregar") {
+          servicio = "actualizarEscuela";
+          metodo = "PUT";
+        }
+
+        var peticionProcesarEscuela = new XMLHttpRequest();
+        peticionProcesarEscuela.open(metodo, oj.gWSUrl() + "escuelas/" + servicio, false);
+        peticionProcesarEscuela.onreadystatechange = function () {
+          if (this.readyState === 4) {
+            if (this.status === 200) {
+              var respuestaJSON = JSON.parse(this.responseText);
+              if (respuestaJSON.hasOwnProperty("error")) {
+                alert('Error, por favor revisa tus datos.');
+              } else {
+                if (respuestaJSON.status === 'exito') {
+                  if (self.botonDialogoEscuela() === "Agregar") {
+                    alert('Escuela agregada correctamente.');
+                  } else {
+                    alert('Escuela actualizada correctamente.');
+                  }
+                } else {
+                  alert('Error, por favor revisa tus datos.');
+                }
+              }
+            } else {
+              alert("Error interno del servidor, favor de contactar al administrador.");
+            }
+          }
+        };
+
+        peticionProcesarEscuela.send(JSON.stringify(datosEscuelas));
+        self.limpiarDialogoEscuela();
+      };
+
+      self.cerrarDialogoEscuela = function(event) {
+        self.limpiarDialogoEscuela();
+      };
+
       /**
        * Optional ViewModel method invoked when this ViewModel is about to be
        * used for the View transition.  The application can put data fetch logic
