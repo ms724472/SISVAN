ea73dc83bf3722eab754abf9805fc2f1e9ef6669
Edgar Tiburcio <edgar.tiburcio@oracle.com>
2020-11-08 23:42:28 -0600

    Completando logica del empaquetado

--- scripts/hooks/after_build.js
+++ scripts/hooks/after_build.js
@@ -3,7 +3,33 @@ const fs = require('fs');
 const archiver = require('archiver');
 module.exports = function (configObj) {
   return new Promise((resolve, reject) => {
-   console.log("Running after_build hook.");
-   resolve();
-     });
+	console.log("Empaquetando archivo de despliegue.");
+   
+	//Aqui definimos el archivo a desplegar en WebLogic es
+	//un archivo ZIP con extencion WAR.
+    var output = fs.createWriteStream('SISVAN.war');    
+    var archive = archiver('zip');
+  
+    output.on('close', () => {
+      console.log('Empaquetado correctamente.');
+      resolve();
+    });
+  
+    archive.on('warning', (error) => {
+      console.warn(error);
+    });
+  
+    archive.on('error', (error) => {
+      reject(error);
+    });
+  
+    archive.pipe(output);
+    archive.directory('web', 'sve');
+	archive.directory('WEB-INF', 'WEB-INF');
+	archive.directory('imagenes', 'imagenes');
+	archive.append(fs.createReadStream('estilos.css'), { name: 'estilos.css'});
+	archive.append(fs.createReadStream('login.jsp'), { name: 'login.jsp'});
+	archive.append(fs.createReadStream('sve.jsp'), { name: 'sve.jsp'});
+	archive.finalize();
+   });
 };
